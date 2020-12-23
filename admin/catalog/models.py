d2dc042bd5f6fb4a4495a84a118e92c711b1ebb5
sarwar sikder <sarwar@workspaceit.com>
2020-12-23 12:30:03 +0600

    Initial commit

---
+++ admin/catalog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    description = models.TextField(default=None)
+    meta_title = models.CharField(max_length=255, null=True)
+    meta_description = models.CharField(max_length=255, null=True)
+    meta_keyword = models.CharField(max_length=255, null=True)
+
+    class Meta:
+        indexes = [
+            models.indexes(fields=['name'])
+        ]
