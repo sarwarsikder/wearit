74faecf60a012eec38ba47fd314e3af4daf1c614
sarwar sikder <sarwar@workspaceit.com>
2020-12-31 15:30:18 +0600

    #5
    Category model migrations changes

--- admin/catalog/models.py
+++ admin/catalog/models.py
@@ -9,7 +9,8 @@ class Category(models.Model):
     meta_title = models.CharField(max_length=255, null=True)
     meta_description = models.CharField(max_length=255, null=True)
     meta_keyword = models.CharField(max_length=255, null=True)
-    meta_keyword = models.CharField(max_length=255, null=True)
+    created_date = models.CharField(max_length=255, null=True)
+    updated_date = models.CharField(max_length=255, null=True)
 
     class Meta:
         indexes = [
