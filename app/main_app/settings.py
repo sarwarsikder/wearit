55e28e05c1f0aa0f074a8a5084c5255f480db46d
sarwar sikder <sarwar@workspaceit.com>
2021-01-06 18:46:27 +0600

    database configuration updated

--- app/main_app/settings.py
+++ app/main_app/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '6(ooi_4^acj(k$(@p+40!g#hmlvcz+n$8%kvz%q^mx474%iu&w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*','.herokuapp.com']
 
 # Application definition
 
