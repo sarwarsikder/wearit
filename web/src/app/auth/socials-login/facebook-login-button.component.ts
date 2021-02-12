9779da82871d402cb93b031afa8926af943e1b7e
AR tanveer <fl.tanveer@gmail.com>
2021-02-12 11:32:02 +0600

    conflict merge

--- web/src/app/auth/socials-login/facebook-login-button.component.ts
+++ web/src/app/auth/socials-login/facebook-login-button.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component } from '@angular/core';
 import { AuthService } from '../../shared/services';
 import { Router } from '@angular/router';
@@ -26,32 +25,3 @@ export class FacebookLoginButtonComponent {
       .catch(err => this.toasty.error(this.translate.instant('Something went wrong, please try again.')));
   }
 }
-=======
-import { Component } from '@angular/core';
-import { AuthService } from '../../shared/services';
-import { Router } from '@angular/router';
-import { SocialAuthService, FacebookLoginProvider } from 'angularx-social-login';
-import { ToastyService } from 'ng2-toasty';
-import { TranslateService } from '@ngx-translate/core';
-
-@Component({
-  selector: 'facebook-login',
-  template: '<button class="btn btn-warning btn-block log-social" (click)="signInWithFacebook()"><i class="fa fa-facebook"></i></button>'
-})
-export class FacebookLoginButtonComponent {
-  private Auth: AuthService;
-
-  constructor(private translate: TranslateService, private router: Router, auth: AuthService, private socialAuthService: SocialAuthService, private toasty: ToastyService) {
-    this.Auth = auth;
-  }
-
-  signInWithFacebook(): void {
-    this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID)
-      .then((resp) => {
-        this.Auth.socialLogin('facebook', resp.authToken);
-        this.router.navigate(['/']);
-      })
-      .catch(err => this.toasty.error(this.translate.instant('Something went wrong, please try again.')));
-  }
-}
->>>>>>> 212d24f73eaf1c856b53f3168db2350cb6296041
--- web/src/app/auth/socials-login/google-login-button.component.ts
+++ web/src/app/auth/socials-login/google-login-button.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component } from '@angular/core';
 import { AuthService } from '../../shared/services';
 import { Router } from '@angular/router';
@@ -25,31 +24,3 @@ export class GoogleLoginButtonComponent {
       }).catch(err => this.toasty.error(this.translate.instant('Something went wrong, please try again.')));
   }
 }
-=======
-import { Component } from '@angular/core';
-import { AuthService } from '../../shared/services';
-import { Router } from '@angular/router';
-import { SocialAuthService, GoogleLoginProvider } from 'angularx-social-login';
-import { ToastyService } from 'ng2-toasty';
-import { TranslateService } from '@ngx-translate/core';
-
-@Component({
-  selector: 'google-login',
-  template: '<button class="btn btn-warning btn-block log-social" (click)="signInWithGoogle()"><i class="fa fa-google"></i></button>'
-})
-export class GoogleLoginButtonComponent {
-  private Auth: AuthService;
-
-  constructor(private translate: TranslateService, private router: Router, auth: AuthService, private socialAuthService: SocialAuthService, private toasty: ToastyService) {
-    this.Auth = auth;
-  }
-
-  signInWithGoogle(): void {
-    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID)
-      .then((resp) => {
-        this.Auth.socialLogin('google', resp.authToken);
-        this.router.navigate(['/']);
-      }).catch(err => this.toasty.error(this.translate.instant('Something went wrong, please try again.')));
-  }
-}
->>>>>>> 212d24f73eaf1c856b53f3168db2350cb6296041
