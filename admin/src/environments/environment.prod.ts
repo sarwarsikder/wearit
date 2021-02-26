a5bfff17fc36030b6a5b7cd1240f654f9d6f3a00
fltanveer <fl.tanveer@gmail.com>
2021-02-26 11:28:44 +0600

    design

--- admin/src/environments/environment.prod.ts
+++ admin/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   version: '1.0.1',
   build: 16,
-  // apiBaseUrl: 'https://wearit-shop-api.herokuapp.com/v1',
-     apiBaseUrl: 'http://localhost:8080/v1',
+   apiBaseUrl: 'https://wearit-shop-api.herokuapp.com/v1',
+    //  apiBaseUrl: 'http://localhost:8080/v1',
   platform: 'admin'
 };
