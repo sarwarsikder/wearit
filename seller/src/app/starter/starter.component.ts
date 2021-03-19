bac9a740308e37f2a561e87b85df0160e515d4dd
sarwar sikder <sarwar@workspaceit.com>
2021-03-19 11:15:18 +0600

    charts added in the seller dashboard

--- seller/src/app/starter/starter.component.ts
+++ seller/src/app/starter/starter.component.ts
@@ -5,6 +5,9 @@ import * as _ from 'lodash';
 
 import { MessageService } from '../message/services/message.service';
 import { StatService } from '../shared/services';
+import { ChartType, ChartOptions, ChartDataSets } from 'chart.js';
+import { SingleDataSet, Label, Color, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip } from 'ng2-charts';
+
 
 @Component({
   templateUrl: './starter.component.html'
@@ -17,8 +20,40 @@ export class StarterComponent implements OnInit {
   public productStat: any = {};
   public saleStat: any = {};
 
+
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public pieChartLabels: Label[] = [['Download', 'Sales'], ['In', 'Store', 'Sales'], 'Mail Sales'];
+  public pieChartData: SingleDataSet = [300, 500, 100];
+  public pieChartType: ChartType = 'pie';
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
+
+
+  public lineChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+  ];
+  public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartOptions: ChartOptions  = {
+    responsive: true,
+  };
+  public lineChartColors: Color[] = [
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(255,0,0,0.3)',
+    },
+  ];
+  public lineChartLegend = true;
+  public lineChartType = 'line';
+  public lineChartPlugins = [];
+
+
   constructor(private shopService: ShopService, private statService: StatService, private toasty: ToastyService,
-    private messageService: MessageService) { }
+    private messageService: MessageService) { 
+      monkeyPatchChartJsTooltip();
+      monkeyPatchChartJsLegend();
+    }
 
   ngOnInit() {
     this.statQuery();
