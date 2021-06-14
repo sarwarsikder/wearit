c9fb6778633c7f5d5776ef4134c89dd0158f6fda
shawonmorshed <shawon@workspaceit.com>
2021-06-14 17:00:22 +0600

    update insta oauth

--- web/src/app/auth/login/login.component.ts
+++ web/src/app/auth/login/login.component.ts
@@ -5,11 +5,12 @@ import { ToastyService } from 'ng2-toasty';
 import { FormGroup, FormBuilder, Validators, FormArray } from "@angular/forms";
 import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http"
 import { Router, ActivatedRoute } from '@angular/router';
-import { environment } from '../../../environments/environment';
-import { tap } from 'rxjs/operators/tap';
+// import { environment } from '../../../environments/environment';
+// import { tap } from 'rxjs/operators/tap';
+// import { HttpClientModule } from '@angular/common/http';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({'Access-Control-Allow-Origin':'*', 'Content-Type': 'application/json' })
 };
 
 @Component({
@@ -27,7 +28,7 @@ export class LoginComponent {
 
   constructor(auth: AuthService, public router: Router, 
     private translate: TranslateService, 
-    private toasty: ToastyService, fb: FormBuilder, private http: HttpClient,
+    private toasty: ToastyService, fb: FormBuilder, public http: HttpClient,
     private route: ActivatedRoute) {
     this.Auth = auth;
 
@@ -54,6 +55,18 @@ export class LoginComponent {
 
 
   }
+
+  ngOnInit() {
+
+    this.route
+             .queryParams
+             .subscribe(params => {
+                 console.log('params: ' + JSON.stringify(params));
+                 let at = params['#access_token'];
+                 console.log('access_token: ' + at);
+             });
+ 
+ }
 
   login() {
     // for (let v in this.login_form.controls) {
@@ -85,29 +98,80 @@ export class LoginComponent {
   }
 
   signInWithInsta(code){
+
+  //   const data = { client_id: '819247062130628',
+  //           redirect_url: 'https://www.google.com',
+  //           scope: 'user_profile,user_media',
+  //           response_type: 'code'
+  //  }
+    
+    // this.http.post('http://localhost:8080/v1/users/insta', data)
+
+    try {
+      const body = new HttpParams()
+        .set('client_id', '819247062130628')
+        .set('redirect_uri', 'https://www.google.com/')
+        .set('scope', 'user_profile,user_media')
+        .set('response_type', 'code');
+  
+      return this.http
+        .post('https://api.instagram.com/oauth/authorize?client_id=819247062130628&redirect_uri=https://www.google.com&scope=user_profile,user_media&response_type=code', null, httpOptions)
+        .subscribe(res => console.log('res', res))
+    } catch (err) {
+      return err;
+    }
+  
+  
+
+    // let url = `https://api.instagram.com/oauth/authorize?client_id=819247062130628&redirect_uri=https://www.google.com/&scope=user_profile,user_media&response_type=code`;
+    // console.log(url);
+    // window.location.href = url;
+
+
+  //   let url = `https://api.instagram.com/oauth/authorize?client_id=819247062130628&redirect_uri=https://www.google.com/&scope=user_profile,user_media&response_type=code`
+
+  //   window.location.href = url
+  //   console.log(window.location.href)
+  //  const data = { client_id: '819247062130628',
+  //           redirect_url: 'https://www.google.com',
+  //           scope: 'user_profile,user_media',
+  //           response_type: 'code'
+  //  }
+
+//   console.log(data)
+
+    // this.http.post<any>('https://api.instagram.com/oauth/authorize',data).subscribe({
+    //     next: data => {
+    //        console.log(data) 
+    //     },
+    //     error: error => {
+    //         console.error('There was an error!', error);
+    //     }
+    // })
+
     // window.location.href = `https://api.instagram.com/oauth/authorize?client_id=${environment.instagramClientId}&redirect_uri=${environment.instagramRedirectUri}&scope=user_profile,user_media&response_type=code`
     //let url = `https://api.instagram.com/oauth/authorize?client_id=${environment.instagramClientId}&redirect_uri=${environment.instagramRedirectUri}&scope=user_profile,user_media&response_type=code`
     // return this.http.get(url)
     // this.router.navigateByUrl(`https://api.instagram.com/oauth/authorize?client_id=${environment.instagramClientId}&redirect_uri=${environment.instagramRedirectUri}&scope=user_profile,user_media&response_type=code`);
     // console.log('Waiting')
     
-      const body = new HttpParams()
-        .set('client_id', environment.instagramClientId)
-        .set('client_secret', environment.instagramClientSecret)
-        .set('grant_type', 'authorization_code')
-        .set('redirect_uri', environment.instagramRedirectUri)
-        .set('code', 'AQCxm9nis2a9FGmj4hcWxMrpBC7pntgfEsMgYABMI-aery039EC_L9J7M8y6i6Gnaxr7otXn5hyjWDDrwm1mxkYt595TBXAUZPHn9ndignZZ2By-3QBeb0zKBVJ5p7FH-2AmT0OxhFID--3E-wUOT0IgpjTasZux48dn_MWkxq8_dxLOK9AX9Xi6BiDJ7HVQ51nRzwEkTvJtPAMLs7uxCWwPz2PWfDwsxHj_VL4mrcIcWA')
-        // .set('auth', 'no-auth');
+      // const body = new HttpParams()
+      //   .set('client_id', "2353717724782027")
+      //   .set('client_secret', "197b319ce7ead80bd914a7237d11b65d")
+      //   .set('grant_type', 'authorization_code')
+      //   .set('redirect_uri', "https://www.google.com/")
+      //   .set('code', code)
+      //   // .set('auth', 'no-auth');
 
-        // console.log('AGAIN')
-         this.http
-        .post('https://api.instagram.com/oauth/access_token', body.toString(), {
-          headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
-        }).pipe(
-          tap(res => {
-            console.log('found?',res)
-          })
-        );
+      //   // console.log('AGAIN')
+      //    this.http
+      //   .post('https://api.instagram.com/oauth/access_token', body.toString(), {
+      //     headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+      //   }).pipe(
+      //     tap(res => {
+      //       console.log('found?',res)
+      //     })
+      //   );
 
         // console.log('ailo?')  
     }
